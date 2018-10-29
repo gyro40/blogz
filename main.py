a374c37ed2997cda1d48b5d9c24c152dc70c40ec
Riley Wood <rileywood40@gmail.com>
2018-10-29 16:27:32 -0500

    fixing

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password2@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'Y768asdFgfdaDSG9'
+
 
 class Blog(db.Model):
 
